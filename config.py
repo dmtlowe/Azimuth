dd3c0a17925e09dc6b58cfcb02c29b6cf3338060
Eduritez <eduritez17@gmail.com>
2024-11-23 17:11:15 +0000

    code

---
+++ config.py
@@ -0,0 +1,18 @@
+MYO_ADDRESS = "DD:31:D8:40:BC:22"
+
+DATA_INPUT_PATH = "data"
+
+MODEL_PATH = "models/model.h5"
+METADATA_PATH = "models/metadata.pkl"
+
+CLASSES = {
+    0: "rest", #saftey
+    1: "closed_fist", #go up
+    2: "ok", #go forwards
+    3: "h", #finger 1
+    4: "e", # finger 2
+    5: "l", # finger 2+3
+    6: "o", # finger 3
+    7: "back", #finger 5
+}
+
